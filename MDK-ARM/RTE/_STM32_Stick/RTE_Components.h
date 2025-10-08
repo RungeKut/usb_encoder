3f28b857ac9af7242c3081275556ae86a5e4de2b
Большаков Сергей Павлович <bolshakov-sp@prima.local>
2025-10-08 10:28:57 +0300

    feat: Debug Config

---
+++ MDK-ARM/RTE/_STM32_Stick/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_Stick' 
+ * Target:  'STM32_Stick' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
